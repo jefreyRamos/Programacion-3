52ef2fb3b00ca5130f106c1b8c0b72a3aaaa0ac3
jefreyramos <jefreyramos011@gmail.com>
2025-04-06 21:13:12 -0500

    Update InvertirLista.exs

--- programacion3/recursividad/tallerListas/InvertirLista.exs
+++ programacion3/recursividad/tallerListas/InvertirLista.exs
@@ -1,5 +1,5 @@
-defmodule InvertirLista do
+defmodule InvertirListas do
   def invertir([]), do: []
   def invertir([h | t]), do: invertir(t) ++ [h]
 end
-IO.inspect(InvertirLista.invertir([1, 2, 3, 4, 5]))
+IO.inspect(s.invertir([1, 2, 3, 4, 5]))
